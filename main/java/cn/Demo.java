1406b0c862bd12653fafdc82e3b6f213f64f8eb3
ws <wangshuo3545@163.com>
2019-01-07 20:40:39 +0800

    1

--- main/java/cn/Demo.java
+++ main/java/cn/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("你好,世界");
         System.out.println("你好啊,勇士");
+        System.out.println("你好啊 辣鸡");
     }
 }
